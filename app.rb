8912811e970f2281a7076cad3b52a3d04b2ea33d
Paul <wheresmyjetpack03@gmail.com>
2016-08-15 16:41:44 -0500

    big commit; lotsa business logic

--- app.rb
+++ app.rb
@@ -1,16 +1,80 @@
 #!/usr/bin/env ruby
 
+require 'io/console'
 require_relative 'lib/core'
 
-circle = GameCircle.new
-goose = Goose.new(name: "Paul", circle: circle)
-
-circle.players = ["A", "b", goose, "d"]
-
-puts "Starting position: "
-puts goose.position
-
-puts
-5.times do 
-  puts goose.pass
+def runner_factory(player, type)
+  type.new(name: player.name)
 end
+
+def starting_position(player, circle)
+  index = circle.find_index(player)
+  raise IndexError if index.to_i >= circle.length - 1
+  rescue StandardError
+    index = circle.length - 2
+  else
+    index
+end
+
+circle = [
+    Duck.new(name: "Paul"), 
+    Duck.new(name: "Mattie"), 
+    Duck.new(name: "Shane"), 
+    Duck.new(name: "Justin"), 
+    Duck.new(name: "Jordy")
+]
+
+circle.each { |p| p.position = circle.find_index(p) }
+
+# get a random player to be the first runner
+selected_player = circle.sample
+game_on = true
+
+while game_on
+  puts "#{selected_player.name} is the runner"
+  start_point = starting_position(selected_player, circle)
+  runner = runner_factory(selected_player, Goose)
+  runner.position = start_point
+  circle.delete(selected_player)
+  puts 
+
+  loop do
+    keypress = STDIN.getch
+    puts "Duck..." if keypress == "\n"
+
+    puts "Standing next to #{circle[runner.position].name}"
+
+    case keypress
+    when " "
+      puts "Duck..."
+      unless runner.position >= circle.length - 1
+        runner.position += 1
+      else 
+        runner.position = 0
+      end
+      puts
+    when "\r"
+      puts "Goose!"
+      puts
+      chaser = circle[runner.position]
+      if chaser.chase >= runner.run
+        puts "#{chaser.name} caught #{runner.name}!"
+        puts "#{runner.name} is still the runner, starting a new round"
+        puts
+      else
+        puts "#{runner.name} made it back to #{chaser.name}'s spot safely!"
+        circle << selected_player
+        selected_player = chaser
+        runner = nil
+        break
+      end
+    when "q"
+      game_on = false
+      break
+    else
+      puts "Not a valid option"
+    end
+  end
+end
+
+puts "Game over"
--- lib/core.rb
+++ lib/core.rb
@@ -1,82 +1,28 @@
-class Player
-end
-
-
-class Duck < Player
+class Duck
   attr_reader :name
-
-  def initalize(args)
-    @name = args[:name]
-  end
-
-end
-
-
-class Goose < Player
-  attr_reader :name, :circle, :position
+  attr_accessor :position
 
   def initialize(args)
     @name = args[:name]
-    @circle = args[:circle]
     @base_speed = 1
   end
 
-  public
-  def pass
-    next_player
-  end
-
-  def tag
-    player_at_position.selected
-    run
-  end
-
-  def position
-    @position ||= start_position
-  end
-
-  private
-  def start_position
-    start = circle.position_of(self)
-    leave_circle
-    start
-  end
-
-  def leave_circle
-    players_in_circle.delete(self)
-  end
-
-  def run
-    @base_speed += rand(0..2)
-  end
-
-  def next_player
-    unless position + 1 > players_in_circle.size - 1
-      @position += 1
-    else
-      @position = 0
-    end
-  end
-
-  def player_at_position
-    players_in_circle[position]
-  end
-
-  def players_in_circle
-    circle.players
+  def chase
+    @base_speed + rand(-1..5)
   end
 end
 
 
-class GameCircle
-  attr_accessor :players
+class Goose
+  attr_reader :name
+  attr_accessor :position
 
-  def vacate(player)
-    players.map! { |p| p.eq?(player) ? nil : p }    
-    player
+  def initialize(args)
+    @name = args[:name]
+    @base_speed = 2
   end
-  
-  def position_of(player)
-    players.index(player)
+
+  def run
+    @base_speed + rand(0..3)
   end
 end
